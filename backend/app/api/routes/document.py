15f7916729eabaf2b84ca058f883915c70c8228d
rahullpanditaa <rpandita1999@gmail.com>
2026-05-06 12:18:09 +0530

    create api router to group all /documents path operations

---
+++ backend/app/api/routes/document.py
@@ -0,0 +1,22 @@
+from app.db.session import get_db
+from app.models.document import Document
+from app.schemas.document import DocumentCreate, DocumentResponse
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+# group together /documents path operations
+router = APIRouter(prefix="/documents", tags=["documents"])
+
+@router.post("/", response_model=DocumentResponse)
+def create_document(payload: DocumentCreate, db: Session = Depends(get_db)):
+    document = Document(filename=payload.filename)
+
+    db.add(document)
+    db.commit()
+    db.refresh(document)
+
+    return document
+
+@router.get("/", response_model=list[DocumentResponse])
+def get_documents(db: Session = Depends(get_db)):
+    return db.query(Document).all()
