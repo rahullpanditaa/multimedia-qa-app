704e7d603afd0df9fe2054b3dd9e16ab41ecf979
rahullpanditaa <rpandita1999@gmail.com>
2026-05-06 13:06:46 +0530

    update document model

--- backend/app/models/document.py
+++ backend/app/models/document.py
@@ -1,17 +1,32 @@
 from app.db.base import Base
 from datetime import datetime
 from sqlalchemy import DateTime, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-# data model representing documents
+# Table for uploaded docs
 class Document(Base):
     __tablename__ = "documents"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
 
+    # original uploaded filename
     filename: Mapped[str] = mapped_column(String, nullable=False)
+
+    # actual saved location on disk
+    filepath: Mapped[str] = mapped_column(String, nullable=False)
+
+    # MIME type of uploaded file
+    # eg - application/pdf
+    mime_type: Mapped[str] = mapped_column(String, nullable=False)
 
     created_at: Mapped[datetime] = mapped_column(
         DateTime,
         default=datetime.utcnow,
+    )
+
+    # Relationship - one doc can have many chunks
+    chunks = relationship(
+        "Chunk",
+        back_populates="document",
+        cascade="all, delete-orphan",
     )
