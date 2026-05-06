1930bf160e783680619b975a57373321972b1c28
rahullpanditaa <rpandita1999@gmail.com>
2026-05-06 16:15:14 +0530

    create service (function) to create embeddings

---
+++ backend/app/services/embedding_service.py
@@ -0,0 +1,41 @@
+"""
+This service generates embeddings using Ollama.
+
+Embeddings convert text into vectors that capture semantic meaning.
+These vectors enable semantic similarity search.
+
+Text embedding model used- nomic-embed-text
+"""
+
+import requests
+
+
+# Ollama embeddings endpoint
+OLLAMA_URL = "http://localhost:11434/api/embeddings"
+
+EMBEDDING_MODEL = "nomic-embed-text"
+
+
+def generate_embedding(text: str) -> list[float]:
+    """
+    Generate embedding vector using Ollama.
+
+    Args:
+        text: Input text
+
+    Returns:
+        Vector embedding
+    """
+
+    response = requests.post(
+        OLLAMA_URL,
+        json={
+            "model": EMBEDDING_MODEL,
+            "prompt": text,
+        },
+    )
+
+    response.raise_for_status()
+    data = response.json()
+
+    return data["embedding"]
