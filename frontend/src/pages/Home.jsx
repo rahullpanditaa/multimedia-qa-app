0babba667dcdfed95f4db1376c85c91d4ba3bf62
rahullpanditaa <rpandita1999@gmail.com>
2026-05-06 20:36:20 +0530

    file upload and chat box component mounted into app

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import FileUpload from "../components/FileUpload";
 import ChatBox from "../components/ChatBox";
 
+// Home Page
 function Home() {
   return (
     <div>
       <h1>Multimedia RAG Application</h1>
 
       <FileUpload />
+
+      <hr />
 
       <ChatBox />
     </div>
