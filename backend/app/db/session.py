5be645b2018c356892d8d7de784462c83bec1cfd
rahullpanditaa <rpandita1999@gmail.com>
2026-05-06 12:14:00 +0530

    setup config.py (for env vars); base.py; session.py (create db engine via db url)

---
+++ backend/app/db/session.py
@@ -0,0 +1,19 @@
+from app.core.config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# create db engine
+engine = create_engine(settings.DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
